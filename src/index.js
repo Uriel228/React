631fb1316995bf9e6a626290e1e26eed7f40597b
Uriel Yasuhiro  Aguilar Rosales <urielol@la-MacBook-Air-de-Uriel.local>
2021-05-12 07:12:08 -0500

    header

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import HelloWorld from './components/HelloWorld.jsx';
-ReactDOM.render(<HelloWorld />, document.getElementById('app'));
+import App from './containers/App.jsx'
+ReactDOM.render(<App />, document.getElementById('app'));
 
 
