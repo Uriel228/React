56b693067a43c411b98cc425be90f237b47bb687
Uriel Yasuhiro  Aguilar Rosales <urielol@la-MacBook-Air-de-Uriel.local>
2021-05-12 07:18:33 -0500

    Comit

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
-import Header from '../components/Header'
+import Header from '../components/Header';
+
 const App = () => (
+
 <div className="App">
-<Header />
+    <Header />
+
 </div>
 );
 
