9f6f3a4564f79e8a0ef94d4a3bb8897bc56d9a20
Uriel Yasuhiro  Aguilar Rosales <urielol@la-MacBook-Air-de-Uriel.local>
2021-05-13 21:51:57 -0500

    Componentes de Carrusel

---
+++ src/components/Categories.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import '../assets/styles/components/Categories.scss';
+const Categories = ({ Children }) => (
+    <div className="categories">
+        <h3 className="categories__title">Mi lista</h3>
+        {Children}
+    </div>
+);
+
+export default Categories;
